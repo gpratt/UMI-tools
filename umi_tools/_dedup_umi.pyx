955209b0ea186ae2948407118d36d4c33d5b9de5
Ian Sudbery <i.sudbery@sheffield.ac.uk>
2017-01-22 15:08:53 +0000

    mixed tabs and spaces

--- umi_tools/_dedup_umi.pyx
+++ umi_tools/_dedup_umi.pyx
@@ -10,6 +10,6 @@ cpdef int edit_distance(a, b, t):
     for k from 0 <= k < l:
         if aa[k] != bb[k]:
             c += 1
-	    if c > t:
-	        return c
+            if c > t:
+                return c
     return c
