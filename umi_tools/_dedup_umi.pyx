f5d8eddb0739457f2909dada3d5a1a69a0b7e3e6
IanSudbery <ian.sudbery@dpag.ox.ac.uk>
2017-01-23 10:36:31 +0000

    two edit_distance functions. One that returns a bool

--- umi_tools/_dedup_umi.pyx
+++ umi_tools/_dedup_umi.pyx
@@ -1,4 +1,5 @@
-cpdef int edit_distance(a, b, t):
+
+cpdef bint threshold_edit_distance(a, b, t):
     cdef char * aa = a
     cdef char * bb = b
     cdef thresh = t
@@ -11,5 +12,19 @@ cpdef int edit_distance(a, b, t):
         if aa[k] != bb[k]:
             c += 1
             if c > thresh:
-                return c
+                return <bint>0
+    return <bint>1
+
+
+cpdef int edit_distance(a, b):
+    cdef char * aa = a
+    cdef char * bb = b
+    cdef int k, l, c
+
+    c = 0
+
+    l = len(a)
+    for k from 0 <= k < l:
+        if aa[k] != bb[k]:
+            c += 1
     return c
